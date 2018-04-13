5e8de89ad42087d64146765cee5fe4090a2fe254
k21700196 <hea7427@naver.com>
2018-04-13 12:05:29 +0900

    Modified simple.c

--- example/simple.c
+++ example/simple.c
@@ -9,8 +9,8 @@
  */
 
 static const char *JSON_STRING =
-	"{\"user\": \"johndoe\", \"admin\": false, \"uid\": 1000,\n  "
-	"\"groups\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
+	"{\"user\": \"haerin\", \"admin\": true, \"uid\": 100,\n  "
+	"\"groups\": [\"person\", \"student\", \"hgu\", \"oss\"]}";
 
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
